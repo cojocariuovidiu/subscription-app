3ef2b0f6ddfad35abc8750731b83c88e6a93ca22
Carlos Martínez <carlosmartcast@gmail.com>
2017-11-18 12:21:53 +0100

    Subscriptions factory: implemented create subscription

--- client/app/subscriptions/subscriptions.factory.js
+++ client/app/subscriptions/subscriptions.factory.js
@@ -6,9 +6,11 @@
         .module('subscriptionApp.subscriptions')
         .factory('subscriptionsFactory', subscriptionsFactory);
 
-    subscriptionsFactory.$inject = [];
+    subscriptionsFactory.$inject = [
+      'PublicRestangular'
+    ];
 
-    function subscriptionsFactory() {
+    function subscriptionsFactory(PublicRestangular) {
 
       return {
         create: create
@@ -17,7 +19,9 @@
       ////
 
       function create(subscription) {
-        return null;
+        return PublicRestangular
+          .all('subscriptions')
+          .post(subscription, null);
       }
 
     }
