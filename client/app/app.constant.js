2dcd22b8a4e2f2d847a6654071d05b70a66804be
Carlos Martínez <carlosmartcast@gmail.com>
2017-11-19 22:00:18 +0100

    app.constant.js updated

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -5,7 +5,7 @@ angular.module('subscriptionApp.constants', [])
 
 .constant('SHARED_CONFIG', {subscriptionSettings:{minAge:16}})
 
-.constant('APP_CONFIG', {apiEndpoint:'http://localhost:9000',newsletterId:'qwerty'})
+.constant('APP_CONFIG', {apiEndpoint:'http://localhost:9000',apiKey:'9cf09931e7cf2d7d36ad06',apiSecret:'ead67e06054b717adb50899be53d3bb41f6452374820',newsletterId:'qwerty'})
 
 ;
 })(angular);
